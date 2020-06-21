ebd217246d272e90733968b644ed6d6c7f095508
Gautam Bhaskar <gautambhaskar@outlook.com>
2020-06-21 16:27:32 -0500

    new stuff

--- school_graph.py
+++ school_graph.py
@@ -50,10 +50,9 @@ while(True):
         #Creats the classes for each period where each student is randomly assigned#
         for x in var_list:
             globals()[x]=[]
-            for b in range(size):
-                node=random.choice(L)
-                globals()[x].append(node)
-                L.remove(node)
+            new_class = random.sample(L, size)
+            globals[x].append(list(new_class))
+            L = list(set(L).difference(set(new_class))) # Nodes of new_class removed from group
 
 
         #adds edges to create a complete graph within each new class#
